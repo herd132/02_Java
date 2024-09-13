31573c01f0cc8c2f8a4b61f3b0677055c481a5c8
herd132 <dydcksdlsla@naver.com>
2024-09-13 17:50:40 +0900

    컬렉션

---
+++ 00_javaBasic/src/edu/kh/basic/test1.java
@@ -0,0 +1,22 @@
+package edu.kh.basic;
+
+import java.util.Scanner;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		
+		String cc = "";
+		int dd=50;
+		switch(dd) {
+		case 50: cc+='x'; break;
+		case 30: cc+='y'; break;
+		default: cc+='z';
+		
+		}
+		System.out.println(cc);
+			
+		
+		
+	}
+}
